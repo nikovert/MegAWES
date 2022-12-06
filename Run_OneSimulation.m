6dbc799b91ce977da2b95f52cc659c19724f6612
Nikolaus Vertovec <niko.vertovec@outlook.com>
2022-12-06 21:28:26 +0000

    working version
    windspeed 30,
    Ft_max = 1.26e06
    amin = -30
    amax = 30
    barrier success, without is rupture!

--- Run_OneSimulation.m
+++ Run_OneSimulation.m
@@ -33,7 +33,7 @@ clc                                 % Clear command window
 Kite_DOF = 6; % Kite degrees of freedom: 3 (point-mass) or 6 (rigid-body)
 % 3DoF: Forced to 22m/s
 % 6DoF: 8m/s  10m/s  14m/s  16m/s  18m/s  20m/s  22m/s  25m/s  28m/s  30m/s
-windspeed = 22; 
+windspeed = 30; 
 
 [act, base_windspeed, constr, DE2019, ENVMT, Lbooth, ...
 	loiterStates, params, simInit, T, winchParameter] = ...
